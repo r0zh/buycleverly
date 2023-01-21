72e72bf384b25c90fbfcb3ce2b687595bf908add
albert0moreno <76948093+albert0moreno@users.noreply.github.com>
2023-01-21 21:17:53 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,21 +1,33 @@
 
+//FUNCIONES BOTÓN Y MOUSE ON DE PRODUCTOS
+
+
 // SI PULSAS EN EL BOTÓN APARECEN LOS SUBAPARTADOS 
 
+var state = ""; // Variable para saber que función se ha ejecutado
+
 function dropdown() {
+    state="onclick"; //Si se ejecuta dropdown con esta condición la variable state almacena el texto "onclick"
     var x = document.getElementById("subapartados");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
+        state=""; //Si se ejecuta el dropdown con esta condición la variable state almacena ""
     }
 }
 
 // SI PONES EL MOUSE ENCIMA DE LA CATEGORÍA APARECEN LOS APARTADOS
-function dropdown2() {
-    document.getElementById("subapartados").style.display="block";
+function dropdown2(){
+    if(state!="onclick"){ // Si la variable state no tiene almacenado el texto "onclick"
+        state = "mouseover";
+        document.getElementById("subapartados").style.display="block";
+    }
 }
 
 // SI QUITAS EL MOUSE DE LA CATEGORÍA DESAPARECEN LOS APARTADOS
 function dropup(){
+    if(state==="mouseover"){ // Si la variable state tiene almacenado el texto "onclick"
     document.getElementById("subapartados").style.display="none";
+    }
 }
